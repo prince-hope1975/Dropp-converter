40ecf2d6d494757a909d3893c0af782c2b98836a
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-06-30 23:04:48 +0100

    deleting

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-var docxConverter = require('docx-pdf');
 var app = express();
 const libre = require('libreoffice-convert');
 const bodyParser = require('body-parser')
@@ -9,8 +8,6 @@ const path = require('path')
 const extend_pdf = '.pdf';
 const extend_docx = '.docx';
 const process = require('process')
-const mv = require('mv');
-
 const port = process.env.PORT || 4000;
 app.use(bodyParser.urlencoded({
     extended: true
@@ -39,7 +36,7 @@ app.post('/uploads/docx', (req, res)=>{
          }
      } catch (err) {
          console.log(err);
-    }
+     }
       file.mv( __dirname + '/uploads/' + name,  (err)=>{
          if(err){
             console.log(err)
@@ -49,16 +46,20 @@ app.post('/uploads/docx', (req, res)=>{
              var initialPath = path.join(__dirname, `/uploads/${First_name}${extend_docx}`);
              var upload_Path = path.join(__dirname, `/uploads/${First_name}${extend_pdf}`);
 
-             docxConverter(initialPath, upload_Path, (err, result)=>{
-              if(err){
-                 console.log(err)
+            const filew = fs.readFileSync(initialPath);
+            libre.convert(filew, extend_pdf, undefined, (err, done) => {
+                if (err) {
+                    console.log(`Error converting file: ${err}`);
               }
               else{
-               res.sendFile(path.resolve(__dirname + '/pages/down_html.html'))
+                fs.writeFileSync(upload_Path, done);
+                res.sendFile(path.resolve(__dirname + '/pages/down_html.html'))
               }
-             })
+            })
          }
       })
+
+
    }
    else{
       res.send('No file selected');
@@ -71,8 +72,8 @@ app.get('/download',(req, res)=>{
         console.log(err)
       }
       else{
-       const delete_path_doc = process.cwd() + `/uploads/${down_name}${extend_docx}`;
-       const delete_path_pdf = process.cwd() + `/uploads/${down_name}${extend_pdf}`;
+       const delete_path_doc = process.cwd() + `/uploads/${First_name}${extend_docx}`;
+       const delete_path_pdf = process.cwd() + `/uploads/${First_name}${extend_pdf}`;
       }
       try{
        fs.unlinkSync(delete_path_doc);
