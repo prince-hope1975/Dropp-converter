3676dbb06e6ae848627b64d834b3c1cfaf142a34
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-08-02 23:19:17 +0100

    deleted unused code

--- public/js/index.js
+++ public/js/index.js
@@ -10,24 +10,6 @@ this.addEventListener('click', (e)=> {
   
 })
 
-
-const format = ["mp4", "xlsx", "pptx", "pdf", "docx", "json", "xml", "html", "png"]
-
-const space_1 = document.querySelector('.any1');
-const space_2 = document.querySelector('.any2');
-let i = 0; let x = format.length -1;
- this.addEventListener('load',()=>{
-setInterval(()=>{
-  space_1.innerHTML = format[i]; if(i >= format.length - 1){
-     i = 0;
-  }else{ i++}}, 500);
-
-  setInterval(()=>{ space_2.innerHTML = format[x]; 
-   if(x <= 0){
-     x = format.length -1
-    } else{x--}}, 1000)
- })
-
       
      
 
